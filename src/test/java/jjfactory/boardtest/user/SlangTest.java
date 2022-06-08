4c0a0b996414ec73938166bffb351f25dc1cf8d1
wogud1514 <wogud1514@gmail.com>
2022-06-08 13:29:21 +0900

    test: 전화번호 길이, 하이픈 포함 체크

---
+++ src/test/java/jjfactory/boardtest/user/SlangTest.java
@@ -0,0 +1,7 @@
+package jjfactory.boardtest.user;
+
+public class SlangTest {
+
+    /*TODO
+    *  글 , 댓글 비속어 제한*/
+}
---
+++ src/test/java/jjfactory/boardtest/user/signUpValidTest.java
@@ -0,0 +1,52 @@
+package jjfactory.boardtest.user;
+
+
+import jjfactory.boardtest.handler.ex.BusinessException;
+import jjfactory.boardtest.handler.ex.ErrorCode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+public class signUpValidTest {
+
+    @Test
+    @DisplayName("전화번호 길이체크")
+    void test(){
+        String phone = "01092494441";
+        String invalidPhone = "010924944412";
+
+        Assertions.assertThrows(BusinessException.class,() ->{
+            phoneLengthCheck(invalidPhone);
+        });
+    }
+
+    @Test
+    @DisplayName("하이픈 포함 체크")
+    void test2(){
+        String phone = "01092494441";
+        String invalidPhone = "0109249-4441";
+
+        Assertions.assertThrows(BusinessException.class,() ->{
+            hyphenCheck(invalidPhone);
+        });
+    }
+
+    void phoneLengthCheck(String phone){
+        int length = phone.length();
+        if(length != 11) throw new BusinessException(ErrorCode.INVALID_PHONE_LENGTH);
+    }
+
+    void hyphenCheck(String phone){
+        if(phone.contains("-")) throw new BusinessException(ErrorCode.INVALID_TYPE_VALUE);
+    }
+
+    void nameValidate(String username){
+
+    }
+
+    void passwordValidate(String username){
+
+    }
+
+}
