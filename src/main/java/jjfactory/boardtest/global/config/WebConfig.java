3a5fdaf6d43da97bb56ea378f78acfa46ef3b20d
jae <wogud1514@naver.com>
2022-07-12 20:50:37 +0900

    feat : 서버단에서 cors 처리

--- src/main/java/jjfactory/boardtest/global/config/WebConfig.java
+++ src/main/java/jjfactory/boardtest/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package jjfactory.boardtest.global.config;
 import jjfactory.boardtest.global.handler.intercepter.HttpInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(httpInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081");
     }
 }
