14294cb4d868293bda75f8fec1a3ab83f00e661c
jae <wogud1514@naver.com>
2022-07-13 21:02:12 +0900

    feat : boardRes 작성자, 작성일 반환하게 수정

--- src/main/java/jjfactory/boardtest/business/dto/board/BoardResponse.java
+++ src/main/java/jjfactory/boardtest/business/dto/board/BoardResponse.java
@@ -5,21 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class BoardResponse {
+    private Long boardId;
+
+    private String userName;
     private String title;
     private String content;
     private Long categoryId;
     private Boolean isView;
     private int likeCount;
+    private String createDate;
 
     public BoardResponse(Board board) {
+        this.boardId = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
         this.categoryId = board.getCategory().getId();
         this.isView = board.getIsView();
         this.likeCount = board.getLikeCount();
+        this.createDate = board.getCreateDate().toString();
+        this.userName = board.getUser().getName();
     }
 }
